019ca79967cbc4b1af8af12abae6dc971682e404
piky <piky@localhost.localdomain>
2019-10-26 11:32:05 -0300

    header responsive

---
+++ css/header.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+            $(window).scroll(function() {
+                        if ($('header').addClass('header2');
+                        }
+                    }
