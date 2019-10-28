ac80f9724df3ea108047e1fce82a281f61696227
NicoDiaz97 <nicoagudiaz97@gmail.com>
2019-10-28 15:17:55 -0300

    en limpio js

--- script.js
+++ script.js
@@ -52,21 +52,3 @@ function contarcaracteres() {
     }, 10);
 
 }
-
-
-
-
-
-
-/* var fn = document.getElementById("nacimiento").value;
-    var fechaactual = new Date();
-    var aact = fechaactual.getFullYear();
-    var mact = fechaactual.getMonth() + 1;
-    var dact = fechaactual.getDate();
-    var nac = fn.split("-");
-    if (aact - nac[0] >= 18) {
-        if (mact >= nac[1]) {
-            if (dact >= nac[2]) { alert("Sos mayor") }
-        }
-    } else alert("Sos menor anda a estudiar bobo.");
-} */
